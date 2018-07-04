1eff22652fb8b1816abf14e7a860e194d1537945
NateCruz <nathahn.delacruz@gmail.com>
2018-07-04 08:47:17 +0200

    commited to new feature

---
+++ HelloWorldEnterprise-web/src/main/java/za/co/openserve/labs/IndexController.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.co.openserve.labs;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Path;
+import org.zkoss.zk.ui.util.GenericForwardComposer;
+import org.zkoss.zul.Label;
+
+/**
+ *
+ * @author DLCruzN
+ */
+public class IndexController extends GenericForwardComposer
+{
+    private Label dude;
+    
+     @Override
+    public void doAfterCompose(Component comp) throws Exception
+    {
+        dude= (Label)Path.getComponent("/window1/label1");
+        dude.setValue("HEY!!!! THIS IS HELLOWORLD MAVEN ENTERPRISE PROJECT");
+    }
+    
+}
